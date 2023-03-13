8967a256637ae8701a95e9152926a0951b6e12a6
munozbiol <munozbiol@gmail.com>
2023-03-13 23:34:41 +0100

    Prueba cambiando previamente el setwd() para adentro de la carpeta correspondiente

---
+++ Indicator Value (IndVal)/dentro de Indval.R
@@ -0,0 +1,6 @@
+# Prueba para dentro de la carpeta de indval 
+
+# Ya estableci primero el working directory dentro de la carpeta de indval
+# previamente estaba en Ecologycal Data Analyses 
+
+setwd("Indicator Value (IndVal)")
